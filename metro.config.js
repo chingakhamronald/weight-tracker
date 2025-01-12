bd97ec59bba33f343da846115ee5362530a296cd
Chingakham Ronaldo <chingakhamronaldo321@gmail.com>
2025-01-12 21:55:14 +0530

    code refactor

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const {assetExts, sourceExts} = defaultConfig.resolver;
-const {withNativeWind} = require('nativewind/metro');
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
